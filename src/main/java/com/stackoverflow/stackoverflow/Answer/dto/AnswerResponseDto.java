246643a7dced50fac17790e65e1d01651ee50c48
김희진 <khj4209kk@gmail.com>
2022-12-22 17:31:24 +0900

    Response에 LocalDateTime 추가

--- src/main/java/com/stackoverflow/stackoverflow/Answer/dto/AnswerResponseDto.java
+++ src/main/java/com/stackoverflow/stackoverflow/Answer/dto/AnswerResponseDto.java
@@ -3,11 +3,14 @@ package com.stackoverflow.stackoverflow.Answer.dto;
 import com.stackoverflow.stackoverflow.Answer.entity.Answer;
 import lombok.*;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Setter
 @AllArgsConstructor
 public class AnswerResponseDto {
     private long answerId;
-
     private String answerBody;
+    private LocalDateTime createdAt;
+    private LocalDateTime modifiedAt;
 }
